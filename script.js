e60366dc15562f58517504686b2262700f96c67d
Lisa Anne Burr <burr_lisa@bah.com>
2017-01-30 14:57:38 -0500

    added jquery logic to retrieve value of button by name

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  // get value assigned to button
+
+  $('button[name="bA1"]').click(function(){
+    var value=$(this).attr("value");
+    console.log(value);
+  });
+  $('button[name="bA2"]').click(function(){
+    var value=$(this).attr("value");
+    console.log(value);
+  });
+  $('button[name="bA3"]').click(function(){
+    var value=$(this).attr("value");
+    console.log(value);
+  });
+  $('button[name="bA4"]').click(function(){
+    var value=$(this).attr("value");
+    console.log(value);
+  });
+})

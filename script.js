659bc057bf6f646390eb7cb9cf1aaae791b095bf
Lisa Anne Burr <burr_lisa@bah.com>
2017-01-31 11:28:27 -0500

    added GET VALUE VARs for all other Qs and As; Added IF ELSE scripts for all other colors but still need to modify the IDs to match the correct color category

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 //get value assigned to a question
 
   var blueQ1=$("#blueQ").attr("value");
+  var pinkQ1=$("#pinkQ").attr("value");
+  var yellowQ1=$("#yellowQ").attr("value");
+  var brownQ1=$("#brownQ").attr("value");
+  var greenQ1=$("#greenQ").attr("value");
+  var orangeQ1=$("#orangeQ").attr("value");
 
 // get value assigned to button
 
@@ -11,6 +16,31 @@ $(document).ready(function() {
   var blueA3=$("#bA3").attr("value");
   var blueA4=$("#bA4").attr("value");
 
+  var pinkA1=$("#pA1").attr("value");
+  var pinkA2=$("#pA2").attr("value");
+  var pinkA3=$("#pA3").attr("value");
+  var pinkA4=$("#pA4").attr("value");
+
+  var yellowA1=$("#yA1").attr("value");
+  var yellowA2=$("#yA2").attr("value");
+  var yellowA3=$("#yA3").attr("value");
+  var yellowA4=$("#yA4").attr("value");
+
+  var brownA1=$("#brA1").attr("value");
+  var brownA2=$("#brA2").attr("value");
+  var brownA3=$("#brA3").attr("value");
+  var brownA4=$("#brA4").attr("value");
+
+  var greenA1=$("#gA1").attr("value");
+  var greenA2=$("#gA2").attr("value");
+  var greenA3=$("#gA3").attr("value");
+  var greenA4=$("#gA4").attr("value");
+
+  var orangeA1=$("#oA1").attr("value");
+  var orangeA2=$("#oA2").attr("value");
+  var orangeA3=$("#oA3").attr("value");
+  var orangeA4=$("#oA4").attr("value");
+
   console.log(blueQ1);
   console.log(blueA1);
   console.log(blueA2);
@@ -18,6 +48,234 @@ $(document).ready(function() {
   console.log(blueA4);
 
 //user clicks button; COMPARE button value to question value; IF equal THEN change feedback to green, ELSE change feedback to red
+
+//BLUE Q & A
+
+$("#bA1").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA1 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA2").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA2 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA3").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA3 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+//PINK Q&A
+
+$("#bA4").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA4 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA1").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA1 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA2").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA2 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA3").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA3 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA4").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA4 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+//YELLOW Q&A
+
+$("#bA4").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA4 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA1").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA1 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA2").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA2 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA3").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA3 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA4").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA4 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+//BROWN Q&A
+
+$("#bA4").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA4 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA1").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA1 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA2").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA2 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA3").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA3 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA4").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA4 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+//GREEN Q&A
+
+$("#bA4").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA4 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA1").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA1 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA2").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA2 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA3").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA3 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+$("#bA4").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA4 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
+
+//ORANGE Q&A
+
+$("#bA4").click(function(){
+  // $("#blueFb").css("background-color", "green");
+  if (blueA4 == blueQ1) {
+    $("#blueFb").css("background-color", "green");
+  } else {
+    $("#blueFb").css("background-color", "red");
+  }
+});
 
 $("#bA1").click(function(){
   // $("#blueFb").css("background-color", "green");
